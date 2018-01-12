63f6cf552981e4162af3cf43eaa1f22276279a34
Nimbles <30619122+nimbles-nl@users.noreply.github.com>
2018-01-12 14:03:43 +0100

    Create SlackException.php

---
+++ src/Nimbles/Slack/Exception/SlackException.php
@@ -0,0 +1,16 @@
+<?php
+/*
+* (c) Nimbles b.v. <wessel@nimbles.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+namespace Nimbles\Slack\Exception;
+
+/**
+ * Class SlackException
+ */
+class SlackException extends \Exception
+{
+
+}
