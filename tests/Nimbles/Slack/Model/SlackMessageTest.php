0d8f55b764e23e6fb9c572bf2ebab87e663c7c92
Nimbles <30619122+nimbles-nl@users.noreply.github.com>
2018-01-12 15:30:09 +0100

    Create SlackMessageTest.php

---
+++ tests/Nimbles/Slack/Model/SlackMessageTest.php
@@ -0,0 +1,21 @@
+?php
+/*
+* (c) Nimbles b.v. <wessel@nimbles.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+namespace Tests\Nimbles\CMTelecom\Model;
+
+use Nimbles\CMTelecom\Model\Issuer;
+use PHPUnit\Framework\TestCase;
+
+class SlackMessageTest extends TestCase
+{
+    public function testSlackMessage()
+    {
+        $slackMessage = new SlackMessage('You look awesome!', '#test', 'TestBot', 'https://www.awesomeimage.com/image.png');
+        
+        $this->assertSame('You look awesome!', $slackMessage->getMessage());
+    }
+}
